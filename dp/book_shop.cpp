ee3b486e0122250a49fdd668fe49f21196963452
Ashrockzzz2003 <ashrockzzz2003@gmail.com>
2023-11-08 21:50:45 +0530

    book_shop

---
+++ dp/book_shop.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int find_max_pages_to_buy(int n, int x, vector<int> &price, vector<int> &pages) {
+    vector<vector<int>> dp(n + 1, vector<int>(x + 1, 0));
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j <= x; j++) {
+            dp[i][j] = dp[i - 1][j];
+            if (j - price[i - 1] >= 0) {
+                dp[i][j] = max(dp[i][j], pages[i - 1] + dp[i - 1][j - price[i - 1]]);
+            }
+        }
+    }
+
+    return dp[n][x];
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, x;
+    cin >> n >> x;
+
+    vector<int> price(n), pages(n);
+    for (int i = 0; i < n; i++) {
+        cin >> price[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        cin >> pages[i];
+    }
+
+    cout << find_max_pages_to_buy(n, x, price, pages) << endl;
+    return 0;
+}
